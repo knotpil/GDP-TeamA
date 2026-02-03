9387a2508465b41cf18e44402e539854d59b522e
Nate Booth <nbooth5@kent.edu>
2026-02-03 15:18:05 -0500

    Create CustomerController.cs
    This script is currently UNTESTED. This provides a simplification of all 3 of the GoTo scripts. Once tested, those scripts will be erased

---
+++ Assets/Scenes/CustomerAI/testOrdering/Scripts/CustomerController.cs
@@ -0,0 +1,62 @@
+
+		{
+			Debug.LogWarning(“CustomerController: No NavMeshAgent found on “ + gameObject.name);
+		}
+	}
+
+	void Start()
+	{
+		SetDestination();
+	}
+
+	void Update() 
+	{
+		if (agent.enabled)
+		{
+			SetDestination();
+			if(!counterReached && !agent.pathPending && agent.remainingDistance <= agent.StoppingDistance) 
+			{
+				counterReached = true;
+			}
+		}
+	}
+
+	public void SetDestination()
+{
+		if (counterTarget == null)
+		{
+			Debug.LogWarning(“CustomerController: No counter target assigned.”, this);
+			return;
+		}
+
+		if (waitingArea == null)
+		{
+			Debug.LogWarning(“CustomerController: No waiting area assigned.”, this);
+			return;
+		}
+
+		if (exitTarget == null)
+		{
+			Debug.LogWarning(“CustomerController: No exit target assigned.”, this);
+			return;
+		}
+
+		if(!agent.enabled)
+		{
+			Debug.LogWarning(“CustomerController: NavMeshAgent is disabled”, this);
+			return;
+		}
+
+		if(!counterReached)
+		{
+			agent.SetDestination(counterTarget.position);
+		} 
+		else if (shouldGoToWaiting)
+		{
+			agent.SetDestination(walkingArea.position);
+		} 
+		else if (shouldLeave)
+		{
+			agent.SetDestination(exitTarget.position);
+		}
+}
