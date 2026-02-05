2a6a62eb0d64256d9285baaebdc7663c189fd263
Jonathan-Lanham <Jonathanlanham80@gmail.com>
2026-02-04 22:52:05 -0500

    Script added to give instantiated selection border
    Script created that allows for selection shader to be instantiated, with thickness and color editable in the editor

--- Assets/ItemSelectionShader/ApplySelectionShader.cs
+++ Assets/ItemSelectionShader/ApplySelectionShader.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class ApplySelectionShader : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Renderer mesh;
+    public Material material;
+    
+    [Range(1, 2)] public float outlineThickness= 1.1f;
+    public Color outlineColor = Color.limeGreen;
+    
+    private MaterialPropertyBlock materialProperty;
+    
+    private static readonly int OutlineThicknessID = Shader.PropertyToID("_OutlineThickness");
+    private static readonly int OutlineColorID = Shader.PropertyToID("_OutlineColor");
+    
     void Start()
     {
+        if (mesh == null) return;
         
-    }
+        if (materialProperty == null) materialProperty = new MaterialPropertyBlock();
+        
+        var materials = mesh.sharedMaterials;
 
-    // Update is called once per frame
-    void Update()
-    {
+        var newMaterials = new Material[materials.Length + 1];
+        for(int i = 0; i < materials.Length; ++i)
+        {
+            newMaterials[i] =  materials[i];
+        }
         
+        newMaterials[materials.Length] = Instantiate(material);
+        
+        mesh.sharedMaterials = newMaterials;
+        
+        mesh.GetPropertyBlock(materialProperty);
+        
+        materialProperty.SetFloat(OutlineThicknessID, outlineThickness);
+        materialProperty.SetColor(OutlineColorID, outlineColor);
+        
+        mesh.SetPropertyBlock(materialProperty);
     }
 }
