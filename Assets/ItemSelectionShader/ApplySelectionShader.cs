0520f405d97dbdf950402c0144ca6b2e7e31b6fd
Solaris Verill <Jonathanlanham80@gmail.com>
2026-02-11 11:10:11 -0500

    Outline shader script completed

--- Assets/ItemSelectionShader/ApplySelectionShader.cs
+++ Assets/ItemSelectionShader/ApplySelectionShader.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
+[ExecuteAlways]
 public class ApplySelectionShader : MonoBehaviour
 {
     public Renderer mesh;
-    public Material material;
     
     [Range(1, 2)] public float outlineThickness= 1.1f;
     public Color outlineColor = Color.limeGreen;
@@ -13,25 +13,11 @@ public class ApplySelectionShader : MonoBehaviour
     private static readonly int OutlineThicknessID = Shader.PropertyToID("_OutlineThickness");
     private static readonly int OutlineColorID = Shader.PropertyToID("_OutlineColor");
     
-    void Start()
+    void OnValidate()
     {
         if (mesh == null) return;
         
         if (materialProperty == null) materialProperty = new MaterialPropertyBlock();
-        
-        var materials = mesh.sharedMaterials;
-
-        var newMaterials = new Material[materials.Length + 1];
-        for(int i = 0; i < materials.Length; ++i)
-        {
-            newMaterials[i] =  materials[i];
-        }
-        
-        newMaterials[materials.Length] = Instantiate(material);
-        
-        mesh.sharedMaterials = newMaterials;
-        
-        mesh.GetPropertyBlock(materialProperty);
         
         materialProperty.SetFloat(OutlineThicknessID, outlineThickness);
         materialProperty.SetColor(OutlineColorID, outlineColor);
